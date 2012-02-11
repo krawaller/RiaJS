6744aea2fd1b2318a1a972479389020aaaa79787
lizanne <lizanne@lizannes-Mac-mini.local>
2012-02-11 19:57:16 +0100

    ...

--- application/models/model_crud.js
+++ application/models/model_crud.js
@@ -3,16 +3,16 @@ var model_crud = Backbone.Model.extend
 	CreateData: function(a_type, a_value)
 	{
 	},
-	
+
 	ReadData: function()
 	{
 	},
 
-    UpdateData: function(a_id, a_type, a_value)
+	UpdateData: function(a_id, a_type, a_value)
 	{
 	},
 
-    DeleteData: function(a_id)
+	DeleteData: function(a_id)
 	{
 	}
 });
